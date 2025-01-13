52650bb2796ce55afbd489c89c260b666ff21896
JoachimKvamme <kaj082024@gmail.com>
2025-01-13 12:03:00 +0100

    Interpreter-klassen startet

---
+++ src/lox/Interpreter.java
@@ -0,0 +1,74 @@
+package lox;
+
+import lox.Expr.Binary;
+import lox.Expr.Grouping;
+import lox.Expr.Literal;
+import lox.Expr.Unary;
+
+class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        // TODO Auto-generated method stub
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+
+                break;
+            
+        }
+
+        // unreachable
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
+            case MINUS:
+                return -(double)right;
+        }
+
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+}
