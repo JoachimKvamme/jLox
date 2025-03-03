e09eb815beed976bd3ea9b20e37ce8d4592b6b27
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-03-03 13:45:38 +0100

    LoxClass, LoxInstance laget

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -313,6 +313,14 @@ class Interpreter implements Expr.Visitor<Object>,
     }
 
     @Override
+    public Void visitClassStmt(Stmt.Class stmt) {
+        environment.define(stmt.name.lexeme, null);
+        LoxClass klass = new LoxClass(stmt.name.lexeme);
+        environment.assign(stmt.name, klass);
+        return null;
+    }
+
+    @Override
     public Void visitIfStmt(Stmt.If stmt) {
         if(isTruthy(evaluate(stmt.condition))) {
             execute(stmt.thenBranch);
