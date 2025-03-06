5ecfa95db808b11b78e37da136166bc621866626
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-03-06 13:24:51 +0100

    Konstruktører og retur i konstruktører

--- src/lox/LoxFunction.java
+++ src/lox/LoxFunction.java
@@ -5,8 +5,10 @@ import java.util.List;
 public class LoxFunction implements LoxCallable {
     private final Stmt.Function declaration;
     private final Environment closure;
+    private final Boolean isInitializer;
 
-    LoxFunction(Stmt.Function declaration, Environment closure) {
+    LoxFunction(Stmt.Function declaration, Environment closure, Boolean isInitializer) {
+        this.isInitializer = isInitializer;
         this.closure = closure;
         this.declaration = declaration;
     }
@@ -14,7 +16,7 @@ public class LoxFunction implements LoxCallable {
     LoxFunction bind(LoxInstance instance) {
         Environment environment = new Environment(closure);
         environment.define("this", environment);
-        return new LoxFunction(declaration, environment);
+        return new LoxFunction(declaration, environment, isInitializer);
     }
 
 
@@ -36,7 +38,7 @@ public class LoxFunction implements LoxCallable {
             return returnValue.value;
         }
 
-        interpreter.executeBlock(declaration.body, environment);
+        if (isInitializer) return closure.getAt(0, "this");
         return null;
     }
 
