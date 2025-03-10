d68aa0e3966583adb0a23e4586f7212be41689e1
Joachim Åsen Kvamme <150397418+JoachimKvamme@users.noreply.github.com>
2025-03-10 13:38:58 +0100

    visitSuperExpr skal være ferdig, men noen problemer med typecasting

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -179,7 +179,17 @@ public class Interpreter implements Expr.Visitor<Object>,
     public Object visitSuperExpr(Expr.Super expr) {
         int distance = locals.get(expr);
         LoxClass superclass = (LoxClass)environment.getAt(distance, "super");
-        
+
+        LoxInstance object = (LoxInstance)environment.getAt(distance - 1, "this");
+
+        LoxFunction method = superclass.findMethod(expr.method.lexeme);
+
+        if (method == null) {
+            throw new RuntimeError(expr.method, "Undefine property '" + expr.method.lexeme + "'.");
+        }
+
+        return method.bind(object);
+
     }
 
     @Override
